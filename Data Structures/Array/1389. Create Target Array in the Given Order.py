8e8021b54057e5ffa10cfc8c6dd08584b5522620
nandiniananthan <57598337+nandiniananthan@users.noreply.github.com>
2020-11-09 20:44:25 -0700

    Add files via upload

---
+++ Data Structures/Array/1389. Create Target Array in the Given Order.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 20:09:41 2020
+
+@author: nandini.ananthan
+"""
+
+nums = [1,2,3,4,0],
+index = [0,1,2,3,0]
+target = []
+
+for i in range(len(nums)):
+    print(i)
+    target.insert(index[i],nums[i])
+    print(target)
