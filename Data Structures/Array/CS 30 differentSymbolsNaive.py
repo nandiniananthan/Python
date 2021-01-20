fac8d3b2be1248a3b6b769d6ceac8b0de1934582
nandiniananthan <57598337+nandiniananthan@users.noreply.github.com>
2021-01-20 09:18:05 +0530

    Add files via upload

---
+++ Data Structures/Array/CS 30 differentSymbolsNaive.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 20:45:33 2021
+
+@author: nandini.ananthan
+"""
+
+s = "cabca"
+OP = 3
+
+def differentSymbolsNaive(s):
+    return len(set(s))
+
+
